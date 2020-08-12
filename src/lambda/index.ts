56d42ae6504ce15ce36bce83bdabb8606518889b
Christoph Gysin <christoph.gysin@gmail.com>
2020-08-12 14:16:02 +0300

    Log event, context

--- src/lambda/index.ts
+++ src/lambda/index.ts
@@ -1,11 +1,14 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'https://deno.land/x/lambda/mod.ts';
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'https://deno.land/x/lambda/mod.ts'
 
-export async function handler( event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  console.log('event:', JSON.stringify(event, null, 2))
+  console.log('context:', JSON.stringify(context, null, 2))
+
   return {
     statusCode: 200,
     headers: {
       'content-type': 'text/html;charset=utf8',
     },
     body: `Welcome to deno ${Deno.version.deno} 🦕`,
-  };
+  }
 }
